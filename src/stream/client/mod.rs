b69d1c66c16331fe48031564d16650d4012ac3e8
Alan <alan.silva_noesis@prestador.globo>
2025-08-10 13:25:42 -0300

    saving progress

--- src/stream/client/mod.rs
+++ src/stream/client/mod.rs
@@ -1,2 +1,5 @@
+#[cfg(feature = "client-async")]
 pub mod r#async;
+
+#[cfg(feature = "client-sync")]
 pub mod sync;
--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -1,15 +1,13 @@
 mod client;
 mod server;
 
-pub use client::r#async::UmbralClient as UmbralAsyncClient;
-pub use client::sync::UmbralClient as UmbralSyncClient;
 pub use server::UmbralServer;
 
 #[cfg(all(feature = "client-async", feature = "client-sync"))]
 compile_error!("features `client-async` and `client-sync` cannot be enabled at the same time");
 
 #[cfg(feature = "client-async")]
-pub type UmbralClient = UmbralAsyncClient;
+pub use client::r#async::UmbralClient;
 
 #[cfg(feature = "client-sync")]
-pub type UmbralClient = UmbralSyncClient;
+pub use client::sync::UmbralClient;
